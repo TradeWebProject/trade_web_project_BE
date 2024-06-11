1b9f95268e76ece67ab5ff51fa049855de58ab15
mmmv41 <tlsalsrud5068@daum.net>
2024-06-12 03:13:17 +0900

    [feat]

--- trade-web-project/src/main/java/com/github/tradewebproject/TradeWebProjectApplication.java
+++ trade-web-project/src/main/java/com/github/tradewebproject/TradeWebProjectApplication.java
@@ -2,12 +2,12 @@ package com.github.tradewebproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class TradeWebProjectApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(TradeWebProjectApplication.class, args);
     }
 
